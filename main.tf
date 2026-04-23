5bb31b9b495bfac481b5a99a88cc59fc08d7b0e9
nandhanapsuresh <nandhanapsuresh75@gmail.com>
2026-04-23 11:07:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 region = "ap-south-1"
 }
 resource "aws_s3_bucket" "demo_bucket" {
-bucket = "jenkins-terraform-demo-nandhana"
+bucket = "jenkins-terraform-demo-nandhana-12345"
 }
 resource "aws_vpc" "demo_vpc" {
 cidr_block = "10.0.0.0/16"
@@ -18,9 +18,11 @@ cidr_block = "10.0.0.0/16"
 resource "aws_subnet" "public_subnet" {
 vpc_id = aws_vpc.demo_vpc.id
 cidr_block = "10.0.1.0/24"
+availability_zone = "ap-south-1a"
 }
 resource "aws_instance" "ec2" {
 ami = "ami-070e5bd3ff10324f8"
 instance_type = "t3.micro"
+subnet_id     = aws_subnet.public_subnet.id
 }
 
