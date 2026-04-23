3299cac793147e798cb1f06e3123a8b96264168c
nandhanapsuresh <nandhanapsuresh75@gmail.com>
2026-04-23 11:10:40 +0530

    Change AWS region and availability zone

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ version = "~> 5.0"
 }
 }
 provider "aws" {
-region = "ap-south-1"
+region = "ap-south-2"
 }
 resource "aws_s3_bucket" "demo_bucket" {
 bucket = "jenkins-terraform-demo-nandhana-12345"
@@ -18,7 +18,7 @@ cidr_block = "10.0.0.0/16"
 resource "aws_subnet" "public_subnet" {
 vpc_id = aws_vpc.demo_vpc.id
 cidr_block = "10.0.1.0/24"
-availability_zone = "ap-south-1a"
+availability_zone = "ap-south-2a"
 }
 resource "aws_instance" "ec2" {
 ami = "ami-070e5bd3ff10324f8"
